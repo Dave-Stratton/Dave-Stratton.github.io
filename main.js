a3ae3add97927c41e381db4fb6a84a1a3284edf7
Dave-Stratton <mlstratton11@gmail.com>
2022-01-26 10:20:20 -0600

    New Updates for Valentines Day (not finished yet)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let scene, camera, renderer, box, heart;
+let scene, camera, renderer, box, heart, box2;
 
 let deg = 0;
 let direction = 0;
@@ -19,13 +19,20 @@ function init() {
     camera.position.set(0,3,7);
 
     renderer.render(scene, camera);
-
-    const boxGeo = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-    const boxMaterial = new THREE.MeshStandardMaterial({color:0xff0000});
+    const opossumBackground = new THREE.TextureLoader().load('/Imgs/OpposumBackground.webp');
+    const boxGeo = new THREE.BoxGeometry(2, 2, 2);
+    const boxMaterial = new THREE.MeshStandardMaterial({color:0xffffff, map:opossumBackground});
     box = new THREE.Mesh(boxGeo, boxMaterial);
-    box.position.set(0,0,0);
+    box.position.set(-3,0,0);
     box.roughness = 0.5;
-    //scene.add(box);
+
+    const box2Geo = new THREE.BoxGeometry(2, 2, 2);
+    const box2Material = new THREE.MeshStandardMaterial({color:0xffffff, map:opossumBackground});
+    box2 = new THREE.Mesh(box2Geo, box2Material);
+    box2.position.set(3,0,0);
+    box2.roughness = 0.5;
+
+
 
     const x = 0, y = 0;
 
@@ -51,6 +58,8 @@ function init() {
     pointLight.position.set(0, 0, 1);
     const lightHelper = new THREE.PointLightHelper(pointLight);
     //scene.add(pointLight, lightHelper);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+    scene.add(ambientLight);
     const gridHelper = new THREE.GridHelper();
     //scene.add(gridHelper);
 }
@@ -62,7 +71,13 @@ function animate() {
     //heart.rotation.y += 0.01;
     //heart.rotation.z += 0.01;
     camera.position.x = 7 * Math.cos(deg);
-    camera.position.z = 7 * Math.sin(deg)
+    camera.position.z = 7 * Math.sin(deg);
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+    box.rotation.z += 0.01;
+    box2.rotation.x += 0.01;
+    box2.rotation.y += 0.01;
+    box2.rotation.z += 0.01;
     camera.lookAt(0,0,0);
     renderer.render(scene, camera);
     if (deg < 3.15 && direction == 0) {
@@ -77,6 +92,11 @@ function animate() {
     else if (deg < 0) {
         direction = 0;
     }
+}
+
+function createOppossum() {
+    scene.add(box);
+    scene.add(box2);
 }
 
 init();
